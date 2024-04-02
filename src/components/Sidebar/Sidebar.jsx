e9fdcd651e54ce4ce79021001cc6527072cdc481
Suman Mondal <mondalsumanrock20@gmail.com>
2024-04-02 19:17:07 +0530

    Recent data added properly

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Sidebar.css";
 import {
   history_icon,
@@ -8,9 +8,17 @@ import {
   question_icon,
   setting_icon,
 } from "../../assets";
+import { Context } from "../../context/Context";
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
+
+  const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
 
   return (
     <div className="sidebar min-h-screen flex flex-col justify-between bg-[#f0f4f9] py-6 px-4">
@@ -21,17 +29,24 @@ const Sidebar = () => {
           className="menu block ml-[10px] cursor-pointer"
           onClick={() => setExtended((prev) => !prev)}
         />
-        <div className="new-chat mt-10 inline-flex items-center gap-2.5 px-2.5 py-2 bg-[#e6eaf1] rounded-full text-[15px] cursor-pointer text-gray-500">
+        <div
+          onClick={() => newChat()}
+          className="new-chat mt-10 inline-flex items-center gap-2.5 px-2.5 py-2 bg-[#e6eaf1] rounded-full text-[15px] cursor-pointer text-gray-500"
+        >
           <img src={plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
         </div>
         {extended ? (
           <div className="recent flex flex-col">
             <p className="recent-title mt-5 mb-4">Recent</p>
-            <div className="recent-entry ">
-              <img src={message_icon} alt="" />
-              <p className="">What is React ...</p>
-            </div>
+            {prevPrompts.map((item, index) => {
+              return (
+                <div onClick={() => loadPrompt(item)} className="recent-entry ">
+                  <img src={message_icon} alt="" />
+                  <p>{item.slice(0, 18)}...</p>
+                </div>
+              );
+            })}
           </div>
         ) : null}
       </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,7 +1,8 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 export const Context = createContext();
+
 const ContextProvider = (props) => {
   const [input, setInput] = useState("");
   const [recentPrompt, setRecentPrompt] = useState("");
@@ -16,38 +17,56 @@ const ContextProvider = (props) => {
     }, 75 * index);
   };
 
+  const newChat = ()=>{
+    setLoading(false);
+    setShowResult(false);
+  }
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
-    setRecentPrompt(input);
-    const response = await runChat(input);
-    let responseArray = response.split("**");
-    let newResponse = "";
-    for (let i = 0; i < responseArray.length; i++) {
-      if (i === 0 || i % 2 !== 1) {
-        newResponse += responseArray[i];
-      } else {
-        newResponse += "<b>" + responseArray[i] + "</b>";
+    
+    try {
+      let response;
+      if (!prompt) {
+        prompt = input;
+        setRecentPrompt(input);
+        setPrevPrompts((prev) => [...prev, input]);
       }
-    }
-    let newResponse2 = newResponse.replace(/\*([^*]+)\*/g, "$1");
-    let newResponseArray = newResponse2.split(/\s+/);
-    newResponseArray = newResponseArray.filter((word) => word !== "");
-    for (let i = 0; i < newResponseArray.length; i++) {
-      const nextWord = newResponseArray[i];
-      if (nextWord === "<b>undefined</b>") {
-        continue;
-      }
-      if (nextWord.endsWith(".")) {
-        delayPara(i, nextWord + "<br/>");
-      } else {
-        delayPara(i, nextWord + " ");
-      }
-    }
+      
+      response = await runChat(prompt);
 
-    setLoading(false);
-    setInput("");
+      let responseArray = response.split("**");
+      let newResponse = "";
+      for (let i = 0; i < responseArray.length; i++) {
+        if (i === 0 || i % 2 !== 1) {
+          newResponse += responseArray[i];
+        } else {
+          newResponse += "<b>" + responseArray[i] + "</b>";
+        }
+      }
+      let newResponse2 = newResponse.replace(/\*([^*]+)\*/g, "$1");
+      let newResponseArray = newResponse2.split(/\s+/);
+      newResponseArray = newResponseArray.filter((word) => word !== "");
+      for (let i = 0; i < newResponseArray.length; i++) {
+        const nextWord = newResponseArray[i];
+        if (nextWord === "<b>undefined</b>") {
+          continue;
+        }
+        if (nextWord.endsWith(".")) {
+          delayPara(i, nextWord + "<br/>");
+        } else {
+          delayPara(i, nextWord + " ");
+        }
+      }
+    } catch (error) {
+      console.error("Error in onSent:", error);
+      // Handle error
+    } finally {
+      setLoading(false);
+      setInput("");
+    }
   };
 
   const contextValue = {
@@ -61,6 +80,7 @@ const ContextProvider = (props) => {
     resultData,
     input,
     setInput,
+    newChat
   };
 
   return (
